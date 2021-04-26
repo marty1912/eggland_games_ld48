268994dd6ea8fc869baaabb3ad8376337c99ea67
Niko Schweighofer <niko.schweighofer1212@gmail.com>
2021-04-26 19:07:29 +0200

    did stuff

--- scannables/Scannable_Static.cs
+++ scannables/Scannable_Static.cs
@@ -4,49 +4,49 @@ using System;
 public class Scannable_Static : StaticBody2D, IScannable
 {
 
-    [Export]
-    float minerals = 100;
+	[Export]
+	float minerals = 100;
 
-    public override void _Ready()
-    {
-        base._Ready();
-        this.setScanState(new ScanStateInactive(this));
-    }
-    public float getMinerals(){
-        return minerals;
-    }
-    ScanState current_scanstate = null;
-    public NodePath getSpriteNode(){
-        return "Sprite";
-    }
-    
-    public Node2D GetMyNode2D(){
-        return (Node2D)this;
+	public override void _Ready()
+	{
+		base._Ready();
+		this.setScanState(new ScanStateInactive(this));
+	}
+	public float getMinerals(){
+		return minerals;
+	}
+	ScanState current_scanstate = null;
+	public NodePath getSpriteNode(){
+		return "Sprite";
+	}
+	
+	public Node2D GetMyNode2D(){
+		return (Node2D)this;
 
-    }
-    public override void _PhysicsProcess(float delta)
-    {
-        base._PhysicsProcess(delta);
-        if (this.current_scanstate != null)
-        {
-            this.current_scanstate.PhysicsProcess(delta);
-        }
-    }
-    public ScanState getScanState(){
-        return this.current_scanstate;
-    }
+	}
+	public override void _PhysicsProcess(float delta)
+	{
+		base._PhysicsProcess(delta);
+		if (this.current_scanstate != null)
+		{
+			this.current_scanstate.PhysicsProcess(delta);
+		}
+	}
+	public ScanState getScanState(){
+		return this.current_scanstate;
+	}
 
-    public void setScanState(ScanState next_state){
-        if(this.current_scanstate != null){
-            current_scanstate.exit();
-        }
+	public void setScanState(ScanState next_state){
+		if(this.current_scanstate != null){
+			current_scanstate.exit();
+		}
 
-        if(this.IsQueuedForDeletion()){
-            return;
-        }
-        current_scanstate = next_state;
-        current_scanstate.enter();
-    }
+		if(this.IsQueuedForDeletion()){
+			return;
+		}
+		current_scanstate = next_state;
+		current_scanstate.enter();
+	}
 
 
 }
--- stuff/FinalButton.cs
+++ stuff/FinalButton.cs
@@ -3,46 +3,46 @@ using System;
 
 public class FinalButton : Area2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
 
 
 
-    [Export]
-    public NodePath SpriteNormalName= "SpriteNormal";
-    [Export]
-    public NodePath SpritePressedName= "SpritePressed";
+	[Export]
+	public NodePath SpriteNormalName= "SpriteNormal";
+	[Export]
+	public NodePath SpritePressedName= "SpritePressed";
 
-    public Sprite spriteNormal;
-    public Sprite spritePressed;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        spriteNormal = (Sprite)GetNode(SpriteNormalName);
-        spritePressed = (Sprite)GetNode(SpritePressedName);
-        spriteNormal.Visible = true;
-        spritePressed.Visible = false;
-    }
-    public void _on_FinalButton_body_entered(Node body){
-        if (body is Boat)
-        {
-        GD.Print("FinalButton enter");
-            spritePressed.Visible = true;
-            spriteNormal.Visible = false;
-            //GlobalEvents.Instance.EmitSignal("FinalButtonEnter");
-        }
-    }
-    public void _on_FinalButton_body_exited(Node body){
-        if (body is Boat)
-        {
-        GD.Print("FinalButton exit");
+	public Sprite spriteNormal;
+	public Sprite spritePressed;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		spriteNormal = (Sprite)GetNode(SpriteNormalName);
+		spritePressed = (Sprite)GetNode(SpritePressedName);
+		spriteNormal.Visible = true;
+		spritePressed.Visible = false;
+	}
+	public void _on_FinalButton_body_entered(Node body){
+		if (body is Boat)
+		{
+		GD.Print("FinalButton enter");
+			spritePressed.Visible = true;
+			spriteNormal.Visible = false;
+			//GlobalEvents.Instance.EmitSignal("FinalButtonEnter");
+		}
+	}
+	public void _on_FinalButton_body_exited(Node body){
+		if (body is Boat)
+		{
+		GD.Print("FinalButton exit");
 
-            spritePressed.Visible = false;
-            spriteNormal.Visible = true;
-            //GlobalEvents.Instance.EmitSignal("FinalButtonExit");
-        }
-    }
+			spritePressed.Visible = false;
+			spriteNormal.Visible = true;
+			//GlobalEvents.Instance.EmitSignal("FinalButtonExit");
+		}
+	}
 
 }
 
