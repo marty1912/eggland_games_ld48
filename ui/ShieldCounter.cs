a5e60acec96b4b4299dd5836357cb172cdfd3955
Martin Bauer <m.bauer@student.tugraz.at>
2021-04-26 21:11:10 +0200

    somestuff added.

--- ui/ShieldCounter.cs
+++ ui/ShieldCounter.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public class ShieldCounter : NinePatchRect
 {
@@ -7,21 +8,52 @@ public class ShieldCounter : NinePatchRect
     // private int a = 2;
     // private string b = "text";
 
+    [Export]
+    public NodePath iconShield0 = "TextureRect";
+
+    [Export]
+    public NodePath iconShield1 = "TextureRect2";
+    [Export]
+    public NodePath iconShield2 = "TextureRect3";
+    [Export]
+    public NodePath iconShield3 = "TextureRect4";
+
+    public List<TextureRect> shieldIcons;
+
     private Label countLabel;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        shieldIcons = new List<TextureRect>();
+        shieldIcons.Add((TextureRect)GetNode(iconShield0));
+        shieldIcons.Add((TextureRect)GetNode(iconShield1));
+        shieldIcons.Add((TextureRect)GetNode(iconShield2));
+        shieldIcons.Add((TextureRect)GetNode(iconShield3));
+
         GetParent().GetNode("UpgradeTree").Connect("buyUpgradeSignal", this, nameof(OnItemBought));
         PlayerStats.Instance.Connect("ShieldCountUpdated", this, nameof(onShieldCountupdate));
         //var shieldUpgrade = GetParent().GetNode("UpgradeTree").GetNode("ShieldUpgrade"); //GetTree().CurrentScene.GetNode("Boat").GetNode("Scanner");
         //shieldUpgrade.Connect("buyItemSignal", this, nameof(OnItemBought));
         countLabel = GetNode<Label>("Count");
+        onShieldCountupdate(0);
     }
 
     private void onShieldCountupdate(int count){
         GD.Print("got notified for shieldcount:", count);
         countLabel.Text = PlayerStats.Instance.ShieldCount.ToString();
+        count = PlayerStats.Instance.ShieldCount;
+        TextureRect[] icons = shieldIcons.ToArray();
+        for (int i = 0; i < icons.Length;i++){
+            icons[i].Visible = false;
+        }
+        for (int i = 0; i < icons.Length;i++){
+            if (i > count)
+            {
+                break;
+            }
+            icons[i].Visible = true;
+        }
     }
     private void OnItemBought(UpgradeTree.UpgradeType type, int value)
     {
